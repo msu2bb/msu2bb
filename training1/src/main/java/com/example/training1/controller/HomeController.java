509bdd0060e267b0d5d3cdbba755801b04b84f85
Kim MinSu <msu2bb@gmail.com>
2017-09-27 11:00:25 +0900

    test upload

---
+++ training1/src/main/java/com/example/training1/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.training1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@RequestMapping("/") 
+	public String home() {
+		return "안녕 Spring Boot!!!!";
+	}
+
+}
